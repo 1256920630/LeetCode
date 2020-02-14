a7ee2ade8a56b3fec721eca9d112a3d6d358cb5f
13660711770 <1256920630@qq.com>
2020-02-14 16:55:39 +0800

    Original

--- src/java/com/code/LeetCode.java
+++ src/java/com/code/LeetCode.java
@@ -3,10 +3,9 @@ package com.code;
 import com.bean.Node;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class LeetCode {
+public abstract class LeetCode {
     //用于临时存储路径
     private StringBuilder sb = new StringBuilder();
     private List<String> res = new ArrayList<>();
@@ -31,12 +30,16 @@ public class LeetCode {
         List<List<String>> lists = new ArrayList<>();
         for (char c : chars) {
             //获取节点
-            lists.add(char2String(c));
+            List<String> char2String = char2String(c);
+            if(char2String!=null){
+                lists.add(char2String);
+            }
         }
 
-
-        //生成树
-        tree(root, lists, 0);
+        if(lists.size()>0){
+            //生成树
+            tree(root, lists, 0);
+        }
 
         return ergodicTree(root);
     }
@@ -78,39 +81,7 @@ public class LeetCode {
     }
 
     //可以使用数据库 动态修改
-    private static List<String> char2String(char c) {
-        List<String> characters = null;
-        switch (c) {
-            case '1':
-                characters = Arrays.asList("");
-                break;
-            case '2':
-                characters = Arrays.asList("a", "b", "c");
-                break;
-            case '3':
-                characters = Arrays.asList("d", "e", "f");
-                break;
-            case '4':
-                characters = Arrays.asList("g", "h", "i");
-                break;
-            case '5':
-                characters = Arrays.asList("j", "k", "l");
-                break;
-            case '6':
-                characters = Arrays.asList("m", "n", "o");
-                break;
-            case '7':
-                characters = Arrays.asList("p", "q", "r", "s");
-                break;
-            case '8':
-                characters = Arrays.asList("t", "u", "v");
-                break;
-            case '9':
-                characters = Arrays.asList("w", "x", "y", "z");
-                break;
-        }
-        return characters;
-    }
+    public abstract List<String> char2String(char c);
 
 
 }
--- test/java/JunitTest.java
+++ test/java/JunitTest.java
@@ -1,4 +1,5 @@
 import com.code.LeetCode;
+import com.code.template.Manual;
 import org.junit.Test;
 
 import java.util.List;
@@ -7,7 +8,7 @@ public class JunitTest {
 
     @Test
     public void test1(){
-        LeetCode t = new LeetCode();
+        LeetCode t = new Manual();
         List<String> strings = t.letterCombinations("23");
         System.out.println(strings.toString());
     }
