e8afb6952ab7425cd690663350a86be49449c34e
1256920630 <48720804+1256920630@users.noreply.github.com>
2020-02-18 15:45:27 +0800

    Update JunitTest.java

--- test/java/JunitTest.java
+++ test/java/JunitTest.java
@@ -12,4 +12,32 @@ public class JunitTest {
         List<String> strings = t.letterCombinations("23");
         System.out.println(strings.toString());
     }
+    
+     @Test
+    public void test2(){
+        LeetCode t = new Manual();
+        List<String> strings = t.letterCombinations("ab");
+        System.out.println(strings.toString());
+    }
+
+    @Test
+    public void test3(){
+        LeetCode t = new Manual();
+        List<String> strings = t.letterCombinations("ab23");
+        System.out.println(strings.toString());
+    }
+
+    @Test
+    public void test4(){
+        LeetCode t = new Manual();
+        List<String> strings = t.letterCombinations("");
+        System.out.println(strings.toString());
+    }
+
+    @Test
+    public void test5(){
+        LeetCode t = new Manual();
+        List<String> strings = t.letterCombinations(null);
+        System.out.println(strings.toString());
+    }
 }
