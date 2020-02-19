646ed5228dc26fbd671bfb3b3fa390a6f8cfc34c
1256920630 <48720804+1256920630@users.noreply.github.com>
2020-02-20 00:50:58 +0800

    Update JunitTest.java

--- test/java/JunitTest.java
+++ test/java/JunitTest.java
@@ -3,41 +3,38 @@ import com.code.template.Manual;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JunitTest {
 
     @Test
     public void test1(){
-        LeetCode t = new Manual();
-        List<String> strings = t.letterCombinations("23");
-        System.out.println(strings.toString());
+        sout("23");
     }
-    
-     @Test
+
+    @Test
     public void test2(){
-        LeetCode t = new Manual();
-        List<String> strings = t.letterCombinations("ab");
-        System.out.println(strings.toString());
+        sout("ab");
     }
 
     @Test
     public void test3(){
-        LeetCode t = new Manual();
-        List<String> strings = t.letterCombinations("ab23");
-        System.out.println(strings.toString());
+        sout("ab23");
     }
 
     @Test
     public void test4(){
-        LeetCode t = new Manual();
-        List<String> strings = t.letterCombinations("");
-        System.out.println(strings.toString());
+        sout("");
     }
 
     @Test
     public void test5(){
+        sout(null);
+    }
+
+    public void sout(String string){
         LeetCode t = new Manual();
-        List<String> strings = t.letterCombinations(null);
-        System.out.println(strings.toString());
+        List<String> strings = t.letterCombinations(string);
+        System.out.println(strings.stream().collect(Collectors.joining(" ")));
     }
 }
